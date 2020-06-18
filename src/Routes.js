b8e7adf98846d02a4e983b2a350baaded51812da
Bismita Guha <bguha@mt.iitr.ac.in>
2020-06-18 19:35:11 +0530

    fix: Fix conflicts

--- src/Routes.js
+++ src/Routes.js
@@ -2,20 +2,26 @@ import React, { Component } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import Login from './components/Login'
 import Register from './components/Register'
-import Info from './components/Info'
-import { login, register, dashboard } from './urls'
+import Dashboard from './components/Dashboard'
+import Form from './components/Form'
+import Submission from './components/Submission'
+import { login, register, dashboard, form, submission } from './urls'
 import {PrivateRoute} from './PrivateRoute'
 import { AuthRoute } from './AuthRoute'
 
 export default class Routes extends Component {
     render() {
         return (
+            <>
             <Switch>
-                <PrivateRoute exact path={dashboard()} component={Info} />
+                <PrivateRoute exact path={dashboard()} component={Dashboard} />
+                <PrivateRoute exact path={form()} component={Form} />
+                <PrivateRoute exact path={submission()} component={Submission} />
                 <AuthRoute path={login()} component={Login} />
                 <AuthRoute path={register()} component={Register} />
                 <Route render={() => <Redirect to='/' />} />    
             </Switch>
+            </>
         )
     }
 }
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Form extends Component {
+    render() {
+        return (
+            <div>
+                forms
+            </div>
+        )
+    }
+}
