d24dce1a2edd604ce6b5ac1c1f4a48ec0d353c82
Bismita Guha <bguha@mt.iitr.ac.in>
2020-06-24 03:19:55 +0530

    feat: Add feature for User Information viewing, filling and updating feat: Add update feature for the User Information fix: Modify POST for User Information to remove automatic state update errors

---
+++ src/components/Profile.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getInfo, postInfo, patchInfo } from '../actions/info'
+import PropTypes from 'prop-types'
+import { Form, Icon, Message } from 'semantic-ui-react'
+import '../styles/Profile.css'
+
+class Profile extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            update: false,
+            error: null,
+            submitted: false
+        }
+    }
+
+    componentDidMount() {
+        this.props.getInfo()   
+    }
+
+    submitInfo = () => {
+        if(!this.state.update) {
+            const data = {
+                name: this.state.name
+            }
+            this.props.postInfo(data, this.callback)
+            this.setState({
+                update: true
+            })
+        }
+        else {
+            const data = {
+                name: this.state.name
+            }
+            this.props.patchInfo(this.props.userinfoid, data, this.callback)
+        } 
+    }
+
+    callback = () => {
+        this.setState({
+            error: this.props.userinfoerror?true:false,
+            submitted: true
+        })
+        setTimeout(() => {
+            this.setState({
+                error: null,
+                submitted: false
+            })
+        }, 5000)
+    }
+
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value});
+    }
+
+    render() {
+        const { userinfo } = this.props
+        {
+            userinfo && userinfo.length === 1 ? this.state.update = true : this.state.update = false
+        }
+        return (
+            <>
+            {
+                userinfo && userinfo.length === 1 ?
+                userinfo.map(userinfo => 
+                    <>
+                    {
+                        this.props.edit === true ?
+                        <Form>
+                            <Form.Input
+                                name="name"
+                                value={this.state.name ? this.state.name : userinfo.name}
+                                onChange={this.onChange}
+                                label='Name'
+                                placeholder='Enter your name...' 
+                                inline
+                            />
+                            <Form.Input
+                                name="role"
+                                value={userinfo.user_type}
+                                label='Role' 
+                                inline
+                                readOnly
+                            />
+                            <Form.Button
+                                disabled={!this.state.name}
+                                onClick={this.submitInfo}
+                            >
+                                SUBMIT
+                            </Form.Button>
+                        </Form>
+                        :
+                        <div>
+                            <span><b>Name: </b>{userinfo.name}</span><br />
+                            <span><b>Email: </b>{userinfo.user.email}</span><br />
+                            <span><b>Role: </b>{userinfo.user_type}</span>
+                        </div>
+                    }
+                    </>
+                    )
+                : 
+                <>
+                {
+                    this.props.edit === true ?
+                    <Form>
+                        <Form.Input
+                            name="name"
+                            value={this.state.name}
+                            onChange={this.onChange}
+                            label='Name'
+                            placeholder='Enter your username...' 
+                            inline
+                        />
+                        <Form.Input
+                            name="role"
+                            value='student'
+                            label='Role' 
+                            inline
+                            readOnly
+                        />
+                        <Form.Button
+                            disabled={!this.state.name}
+                            onClick={this.submitInfo}
+                        >
+                            SUBMIT
+                        </Form.Button>
+                    </Form>
+                    : <span>You have not filled in your basic information till now.</span>
+                }
+                </>
+            }
+            </>            
+        )
+    }
+}
+
+Profile.propTypes = {
+    userinfo: PropTypes.func.isRequired,
+    postInfo: PropTypes.func.isRequired,
+    patchInfo: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+    userinfo: state.info.userinfo,
+    userinfoerror: state.info.userinfoerror,
+    userinfoid: state.info.userinfoid,
+})
+
+export default connect(
+    mapStateToProps,
+    { getInfo, postInfo, patchInfo }
+)(Profile)
--- src/reducers/info.js
+++ src/reducers/info.js
@@ -1,13 +1,15 @@
 import {
     GET_USER_INFO,
     POST_USER_INFO,
-    USER_INFO_ERRORS
+    USER_INFO_ERRORS,
+    UPDATE_USER_INFO
 } from '../actions/types'
 
 const initialState = {
     userinfo: [],
     postuserinfo: [],
     userinfoerror: null,
+    userinfoid: null,
 }
 
 const infoReducer = (state = initialState, action) => {
@@ -16,12 +18,20 @@ const infoReducer = (state = initialState, action) => {
             return {
                 ...state,
                 userinfo: action.payload,
+                userinfoid: action.payload.length !== 0 ? action.payload[0].id : state.userinfoid
             };
         case POST_USER_INFO:
             return {
                 ...state,
                 postuserinfo: action.payload,
-                userinfo: [...state.userinfo, action.payload],
+                userinfo: [action.payload],
+                userinfoid: action.payload.id
+            }
+        case UPDATE_USER_INFO:
+            return {
+                ...state,
+                userinfo: [action.payload],
+                userinfoid: action.payload.id
             }
         case USER_INFO_ERRORS:
             return {
