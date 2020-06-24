d957b66a1f2e4ada9bbe5886276abef883d63235
Bismita Guha <bguha@mt.iitr.ac.in>
2020-06-24 19:09:04 +0530

    style: Modify imports

--- src/AuthRoute.js
+++ src/AuthRoute.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import { dashboard } from './urls'
 
 export const AuthRoute = ({ component: Component, ...rest }) => (
     <>
-    <Navbar login={false}/>
+    <Navbar/>
     <Route {...rest} render={props => (
         localStorage.getItem('token') === null
         ? <Component {...props} />
-        : <Redirect to={dashboard()} />
+        : <Redirect to='/'/>
     )} />
     </>
 )
