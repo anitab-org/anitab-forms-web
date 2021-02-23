c81e35e622d450d3b8ea50114d7d53956543e132
S Jeeva <43230676+jeeva2812@users.noreply.github.com>
2021-02-24 04:39:20 +0530

    fix: incorrect active tab on refresh (#92)

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -11,7 +11,7 @@ class Menubar extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            activeItem: 'dashboard'
+            activeItem: window.location.pathname.substring(1) || 'dashboard'
         }
     }
 
